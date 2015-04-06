130705319cf0a569350a4e0f43e424ea05ac2d06
Zheng-Li <zl714@nyu.edu>
2015-04-05 23:26:31 -0400

    geolocation update

--- geolocation.py
+++ geolocation.py
@@ -6,33 +6,36 @@ import string
 from time import sleep
 from location_reference import get_full_name
 import random
+import sys
+import getopt
 
 URL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' 
 API_KEY = "AIzaSyAW3GX8hrAuoET8ESMA8rB8Y7AXqTHMH6I"
-result = []
 
-def read_file():
-	in_file = csv.reader(open('location.csv', 'rU'))
-	for row in in_file : 
-		# tmp = [row['City'], row['State Full Name'] ,row['State'], row['Country']]
-		result.append(row)
+def read_file(file_name):
+	f = csv.reader(open(file_name, 'rU'))
+	raw_data = []
+	for row in f : 		
+		# Input data in <City, State, Country> order
+		raw_data.append([row[0], row[1], row[2]])
+	return raw_data
 
-def write_file():
-	out_file = csv.writer(open('result.csv', 'wb', buffering=0))
-	for x in result :
-		print (x)  
-		out_file.writerow(x)
+def write_file(file_name, data):
+	f = csv.writer(open(file_name, 'wb', buffering=0))
+	for x in data :
+		f.writerow(x)
 
 def send_request_by_record(record):
-	address = record[1] + ',+' + record[2] + ',+' + record[3] + ',+' + record[4]
+	# Input data in <City, State, Country> order
+	address = record[0] + ',+' + record[1] + ',+' + record[2]
 	address = address.replace(" ", "+")
 	url = URL + address
 	response = urllib2.urlopen(url)
 	jsongeocode = response.read()
 	geo = parse_json(jsongeocode)
-	record[6] = geo[0]
-	record[7] = geo[1]
-	return record
+	# Output data in <City, State, Country, latitude, longitude> order
+	record[3] = geo[0]
+	record[4] = geo[1]
 
 def send_request_by_location(city, state, country) :
 	address = country
@@ -63,12 +66,32 @@ def parse_json(code) :
 		print response['status']
 		return ['0', '0']
 
-if __name__ == '__main__':
-	read_file()
+def main(argv):
+	input_file = ''
+	output_file = ''
+	try:
+		opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+	except getopt.GetoptError:
+		print 'test.py -i <inputfile> -o <outputfile>'
+     	sys.exit(2)
+   	for opt, arg in opts:
+   		if opt == '-h':
+   			print 'test.py -i <inputfile> -o <outputfile>'
+   			sys.exit()
+   		elif opt in ("-i", "--ifile"):
+   			input_file = arg
+   		elif opt in ("-o", "--ofile"):
+   			output_file = arg
+   	print 'Input file is "', input_file
+   	print 'Output file is "', output_file
+
+   	data = read_file(input_file)
 	for item in result :
-		item = send_request(item)
+		item = send_request_by_record(item)
 		print item
-		# import_db(item)
-		sleep(random.uniform(1.6, 3.6))
-	write_file()
+		# sleep(random.uniform(1.6, 3.6))
+	write_file(output_file, data)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
