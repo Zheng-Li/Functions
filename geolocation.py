49b0ae762b21089d80f5b4a73a6cf46680914fc4
Zheng-Li <zl714@nyu.edu>
2015-04-14 15:36:55 -0400

    Selenium update

--- geolocation.py
+++ geolocation.py
@@ -8,6 +8,7 @@ from location_reference import get_full_name
 import random
 import sys
 import getopt
+import gspread
 
 URL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' 
 API_KEY = "AIzaSyAW3GX8hrAuoET8ESMA8rB8Y7AXqTHMH6I"
@@ -29,13 +30,15 @@ def send_request_by_record(record):
 	# Input data in <City, State, Country> order
 	address = record[0] + ',+' + record[1] + ',+' + record[2]
 	address = address.replace(" ", "+")
-	url = URL + address
+	url = URL + address + '&key=' + API_KEY
+	print url
 	response = urllib2.urlopen(url)
 	jsongeocode = response.read()
 	geo = parse_json(jsongeocode)
 	# Output data in <City, State, Country, latitude, longitude> order
-	record[3] = geo[0]
-	record[4] = geo[1]
+	record.append(geo[0])
+	record.append(geo[1])
+	print record
 
 def send_request_by_location(city, state, country) :
 	address = country
@@ -66,32 +69,61 @@ def parse_json(code) :
 		print response['status']
 		return ['0', '0']
 
-def main(argv):
-	input_file = ''
-	output_file = ''
-	try:
-		opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-	except getopt.GetoptError:
-		print 'test.py -i <inputfile> -o <outputfile>'
-     	sys.exit(2)
-   	for opt, arg in opts:
-   		if opt == '-h':
-   			print 'test.py -i <inputfile> -o <outputfile>'
-   			sys.exit()
-   		elif opt in ("-i", "--ifile"):
-   			input_file = arg
-   		elif opt in ("-o", "--ofile"):
-   			output_file = arg
-   	print 'Input file is "', input_file
-   	print 'Output file is "', output_file
+def update_spreadsheet() :
+	gc = gspread.login('zheng@zoomdojo.com', 'marymount05')
+	sh = gc.open('Test Project')
 
-   	data = read_file(input_file)
-	for item in result :
-		item = send_request_by_record(item)
-		print item
-		# sleep(random.uniform(1.6, 3.6))
-	write_file(output_file, data)
+	worksheet = sh.worksheet('States Short_Canada_Australia')
+
+	for i in range(4, 17) :
+		# state = worksheet.acell('B' + str(i)).value
+		# country = 'Canada'
+		# x = ['', state, country]
+		# send_request_by_record(x)
+		# worksheet.update_acell('C' + str(i), x[3])
+		# worksheet.update_acell('D' + str(i), x[4])
+		data = ['', worksheet.acell('A'+str(i)).value, worksheet.acell('B'+str(i)).value, 'Canada', worksheet.acell('C'+str(i)).value, worksheet.acell('D'+str(i)).value]
+		sql = '''INSERT INTO zd_new_location(City, State, Abbreviation, Country, Latitude, Longitude) VALUES (\'{0[0]}\', \'{0[1]}\', \'{0[2]}\', \'{0[3]}\', {1}, {2}) ON DUPLICATE KEY UPDATE State = VALUES(State), Latitude = VALUES(Latitude), Longitude = VALUES(Longitude);'''.format(data, float(data[4]), float(data[5]))
+		print sql
+
+	for j in range(20, 30) :
+		# state = worksheet.acell('B' + str(j)).value
+		# country = 'Australia'
+		# y = ['', state, country]
+		# send_request_by_record(y)
+		# worksheet.update_acell('C' + str(j), y[3])
+		# worksheet.update_acell('D' + str(j), y[4])
+		data = ['', worksheet.acell('A'+str(j)).value, worksheet.acell('B'+str(j)).value, 'Australia', worksheet.acell('C'+str(j)).value, worksheet.acell('D'+str(j)).value]
+		sql = '''INSERT INTO zd_new_location(City, State, Abbreviation, Country, Latitude, Longitude) VALUES (\'{0[0]}\', \'{0[1]}\', \'{0[2]}\', \'{0[3]}\', {1}, {2}) ON DUPLICATE KEY UPDATE State = VALUES(State), Latitude = VALUES(Latitude), Longitude = VALUES(Longitude);'''.format(data, float(data[4]), float(data[5]))
+		print sql
+
+# def main(argv):
+# 	input_file = ''
+# 	output_file = ''
+# 	try:
+# 		opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+# 	except getopt.GetoptError:
+# 		print 'test.py -i <inputfile> -o <outputfile>'
+#      	sys.exit(2)
+#    	for opt, arg in opts:
+#    		if opt == '-h':
+#    			print 'test.py -i <inputfile> -o <outputfile>'
+#    			sys.exit()
+#    		elif opt in ("-i", "--ifile"):
+#    			input_file = arg
+#    		elif opt in ("-o", "--ofile"):
+#    			output_file = arg
+#    	print 'Input file is "', input_file
+#    	print 'Output file is "', output_file
+
+#    	data = read_file(input_file)
+# 	for item in result :
+# 		item = send_request_by_record(item)
+# 		print item
+# 		# sleep(random.uniform(1.6, 3.6))
+# 	write_file(output_file, data)
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+   # main(sys.argv[1:])
+   update_spreadsheet()
 
--- table_parse.py
+++ table_parse.py
@@ -5,6 +5,7 @@ import socket
 import HTMLParser
 import random
 import gspread
+from selenium import webdriver
 from BeautifulSoup import BeautifulSoup
 from time import sleep
 import sys
@@ -61,31 +62,76 @@ def table_parse(page) :
 
 def update_spreadsheet(data) :
 	gc = gspread.login('zheng@zoomdojo.com', 'marymount05')
-	sh = gc.open('Zheng Brass Rings Jobs to upload March 2015')
 
+	# sh = gc.open('Zheng Brass Rings Jobs to upload March 2015')
 	# worksheet = sh.add_worksheet(title="bayer", rows="200", cols="20")
-	worksheet = sh.worksheet('bayer')
+	# worksheet = sh.worksheet('bayer')
+
+	sh = gc.open('Test')
+	worksheet = sh.worksheet('Intel')
+	
 
 	# Header
-	cell_list = worksheet.range('A1:E1')
-	cell_values = ['Job Title', 'Job Url', 'Posted on', 'Country', 'State-City']
-	for i, val in enumerate(cell_values):  #gives us a tuple of an index and value
-	    cell_list[i].value = val    #use the index on cell_list and the val from cell_values
-	worksheet.update_cells(cell_list)
+	# cell_list = worksheet.range('A1:E1')
+	# cell_values = ['Job Title', 'Job Url', 'Posted on', 'Country', 'State-City']
+	# for i, val in enumerate(cell_values):  #gives us a tuple of an index and value
+	#     cell_list[i].value = val    #use the index on cell_list and the val from cell_values
+	# worksheet.update_cells(cell_list)
 
 	# Data
 	for x, row in enumerate(data) :
 		num = x+2
-		cell_list = worksheet.range('A'+str(num)+':F'+str(num))
+		cell_list = worksheet.range('A'+str(num)+':D'+str(num))
 		for i, val in enumerate(row) :
 			cell_list[i].value = val
 		worksheet.update_cells(cell_list)
+
+def parse_javascript(url, page) :
+	# soup = BeautifulSoup(page) 
+	# js  = soup.findAll("script")
+	# for j in js :
+	# 	ss = j.prettify()
+	# 	print ss
+
+	browser = webdriver.Firefox()
+	browser.get(url)
+	for x in range(1, page) :
+		button = browser.find_element_by_id("jobPager").find_elements_by_tag_name("span")[3].find_element_by_tag_name("span").find_element_by_tag_name("a")
+		print button.get_attribute("class")
+		button.click()
+	browser.implicitly_wait(5)
+	table = browser.find_elements_by_tag_name("table")[2]
+
+	# header = []
+	# headers = table.find_element_by_tag_name("thead").find_element_by_tag_name("tr").find_elements_by_tag_name("th")
+	# for h in headers :
+	# 	tmp = h.text
+	# 	header.append(tmp)
+	# print head
+
+	records = []
+	jobs = table.find_element_by_tag_name("tbody").find_elements_by_tag_name("tr")
+	for j in jobs:
+		title = j.find_elements_by_tag_name("td")[1].find_element_by_tag_name("div").find_element_by_tag_name("div").find_element_by_tag_name("div").find_element_by_tag_name("a").text
+		url = j.find_elements_by_tag_name("td")[1].find_element_by_tag_name("div").find_element_by_tag_name("div").find_element_by_tag_name("div").find_element_by_tag_name("a").get_attribute("href")
+		location = j.find_elements_by_tag_name("td")[2].find_element_by_tag_name("div").find_element_by_tag_name("div").find_element_by_tag_name("span").text
+		posted = j.find_elements_by_tag_name("td")[3].find_element_by_tag_name("div").find_element_by_tag_name("div").find_element_by_tag_name("span").text
+		record = [title, url, location, posted]
+		records.append(record)
+		print record
+
+	browser.quit()
+	return records
 
 if __name__ == '__main__':
 	# url = 'https://career.bayer.com/en/career/job-search/?accessLevel=student&functional_area=&country=*&location=&company=&fulltext='
 	url = 'https://intel.taleo.net/careersection/10000/jobsearch.ftl'
 	
 	page = url_parse(url)
-	print page
+	data = []
+	for i in range (1, 11) :
+		data += parse_javascript(url, i)
 	# result = table_parse(page)
-	# update_spreadsheet(result)
+	update_spreadsheet(data)
+
+
