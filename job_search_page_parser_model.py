e968623f3e254d3939bb8cc857ebfff976bc18a4
Zheng-Li <zl714@nyu.edu>
2015-07-01 15:07:16 -0400

    update

--- job_search_page_parser_model.py
+++ job_search_page_parser_model.py
@@ -7,7 +7,6 @@ import csv
 import json
 import gspread
 import time
-from time import sleep
 from File.file import *
 from oauth2client.client import SignedJwtAssertionCredentials
 from Geolocation.geolocation_reference import get_abbreviation
@@ -118,12 +117,12 @@ def parse_job_search_page(browser, keyword, num_of_pages) :
 	# key_search.clear()
 	# key_search.send_keys(key)
 	# key_search.send_keys(Keys.ENTER)
-	# sleep(1)
+	# browser.implicitly_wait(2)
 
 	# ----------- Change number of result per page -------
 	# display = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.NAME, '')))
 	# Select(display).select_by_value('100')
-	# sleep(1)
+	# browser.implicitly_wait(2)
 
 	# ------------ Parse all pages of search result ------------
 	# button_moreJobs = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH,'')))
@@ -131,8 +130,8 @@ def parse_job_search_page(browser, keyword, num_of_pages) :
 	# 	button_moreJobs = browser.find_element_by_xpath('')
 	# 	button_moreJobs.click()
 	# 	button_moreJobs = None
-	# 	sleep(1.7)
-	# sleep(5)
+	# 	browser.implicitly_wait(1.7)
+	# browser.implicitly_wait(2)
 
 	# ------------ Switch to inner iframe if exists ----------
 	# browser.switch_to.frame(browser.find_element_by_tag_name('iframe'))
@@ -144,7 +143,7 @@ def parse_job_search_page(browser, keyword, num_of_pages) :
 			pager = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, '')))  
 			if i != 0 :
 				pager.click()
-			sleep(3)
+			browser.implicitly_wait(3)
 
 			table = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '')))
 			jobs = table.find_elements_by_tag_name('tr')
