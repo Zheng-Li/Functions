9ee112c968feb9a9237b377cb66a9c511cd1f021
Zheng-Li <zl714@nyu.edu>
2015-09-09 12:49:27 -0400

    Clean up

--- job_search_page_parser_model.py
+++ job_search_page_parser_model.py
@@ -24,8 +24,8 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 
-def login(spreadsheet, worksheet) :
-	json_key = json.load(open('zheng-6cef143e8ce1.json'))
+def login(credentials_file, spreadsheet, worksheet) :
+	json_key = json.load(open(credentials_file))
 	scope = ['https://spreadsheets.google.com/feeds']
 
 	credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
@@ -51,93 +51,106 @@ def check_url_status(url) :
 		print 'Unknown error!'
 		return False
 
-def load_remove_keywords () :
-	spreadsheet_name = 'Test Project'
-	worksheet_name = 'Keywords_Jobs to Remove'
-	ws = login(spreadsheet_name, worksheet_name)
+# ++++++++++++ Tagging functions (Deprecated) +++++++++++++
+# def load_remove_keywords () :
+# 	spreadsheet_name = 'Test Project'
+# 	worksheet_name = 'Keywords_Jobs to Remove'
+# 	ws = login(spreadsheet_name, worksheet_name)
 
-	keyword_dict = ws.col_values(1)
-	del keyword_dict[0]
+# 	keyword_dict = ws.col_values(1)
+# 	del keyword_dict[0]
 
-	return keyword_dict
+# 	return keyword_dict
 
-def check_if_exists (title, remove_keyword_dict) :
-	title = title.strip().lower()
-	for ky in remove_keyword_dict :
-		ky_tmp = re.escape(ky)
-		reg = '\\b' + ky_tmp + '[,.]?\\b'
-		result = re.search(reg, title)  
-		if result :
-			return True
-	return False
+# def check_if_exists (title, remove_keyword_dict) :
+# 	title = title.strip().lower()
+# 	for ky in remove_keyword_dict :
+# 		ky_tmp = re.escape(ky)
+# 		reg = '\\b' + ky_tmp + '[,.]?\\b'
+# 		result = re.search(reg, title)  
+# 		if result :
+# 			return True
+# 	return False
 
-def load_tag_keywords () :
-	spreadsheet_name = 'Test Project'
+# def load_tag_keywords () :
+# 	spreadsheet_name = 'Test Project'
 
-	worksheet_name_1 = 'Keywords Page 1 Job Tags Noun'
-	ws_1 = login(spreadsheet_name, worksheet_name_1)
-	keyword_dict_1 = {}
+# 	worksheet_name_1 = 'Keywords Page 1 Job Tags Noun'
+# 	ws_1 = login(spreadsheet_name, worksheet_name_1)
+# 	keyword_dict_1 = {}
 
-	raw_data_1 = ws_1.get_all_values()
-	for row in raw_data_1 :
-		key = row.pop(0).lower().strip()
-		values = filter(None, row)
-		keyword_dict_1[key] = [x.lower() for x in values]
+# 	raw_data_1 = ws_1.get_all_values()
+# 	for row in raw_data_1 :
+# 		key = row.pop(0).lower().strip()
+# 		values = filter(None, row)
+# 		keyword_dict_1[key] = [x.lower() for x in values]
 
-	worksheet_name_2 = 'KKeywords Page 2 Job Tags Adjective'
-	ws_2 = login(spreadsheet_name, worksheet_name_2)
-	keyword_dict_2 = {}
+# 	worksheet_name_2 = 'KKeywords Page 2 Job Tags Adjective'
+# 	ws_2 = login(spreadsheet_name, worksheet_name_2)
+# 	keyword_dict_2 = {}
 
-	raw_data_2 = ws_2.get_all_values()
-	for row in raw_data_2 :
-		key = row.pop(0).lower().strip()
-		values = filter(None, row)
-		keyword_dict_2[key] = [x.lower() for x in values]
+# 	raw_data_2 = ws_2.get_all_values()
+# 	for row in raw_data_2 :
+# 		key = row.pop(0).lower().strip()
+# 		values = filter(None, row)
+# 		keyword_dict_2[key] = [x.lower() for x in values]
 
-	return  keyword_dict_1, keyword_dict_2
+# 	return  keyword_dict_1, keyword_dict_2
 
-def tag_job (title, keyword_dict) :
-	tag_list = []
-	title = title.strip().lower()
-	for ky in keyword_dict.keys() :
-		ky_tmp = re.escape(ky)
-		reg = '\\b' + ky_tmp + '[,.]?\\b'
-		result = re.search(reg, title)  
-		if result :
-			tag_list.append(ky)
-			tag_list += keyword_dict[ky]
+# def tag_job (title, keyword_dict) :
+# 	tag_list = []
+# 	title = title.strip().lower()
+# 	for ky in keyword_dict.keys() :
+# 		ky_tmp = re.escape(ky)
+# 		reg = '\\b' + ky_tmp + '[,.]?\\b'
+# 		result = re.search(reg, title)  
+# 		if result :
+# 			tag_list.append(ky)
+# 			tag_list += keyword_dict[ky]
 
-	tags = list(set(tag_list))
-	return tags
+# 	tags = list(set(tag_list))
+# 	return tags
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 def parse_job_search_page(browser, keyword, num_of_pages) :
 	result = []
 
-	remove_keyword_dict = load_remove_keywords()
-	tag_keyword_dict_1, tag_keyword_dict_2 = load_tag_keywords()
+	# ++++++++++++ Tagging functions (Deprecated) +++++++++++++
+	# remove_keyword_dict = load_remove_keywords()
+	# tag_keyword_dict_1, tag_keyword_dict_2 = load_tag_keywords()
+	# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-	# ------------ Add keyword to search ---------------
+	# ------------ a. Add keyword to search ---------------
 	# key_search = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '')))
 	# key_search.clear()
-	# key_search.send_keys(key)
+	# key_search.send_keys(keyword)
 	# key_search.send_keys(Keys.ENTER)
 	# browser.implicitly_wait(2)
 
-	# ----------- Change number of result per page -------
-	# display = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.NAME, '')))
-	# Select(display).select_by_value('100')
+	# ------------ b. Select checkbox to refine search --------
+	# checkboxes = browser.find_elements_by_xpath('')
+	# for cb in checkboxes : # Choose the ones needed to be checked
+	# 	if cb.is_selected() :
+	# 		cb.click()
+	# button_search = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH, ''))).click()
 	# browser.implicitly_wait(2)
 
-	# ------------ Parse all pages of search result ------------
+	# ------------ c. Parse all pages of search result ------------
 	# button_moreJobs = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH,'')))
 	# while len(browser.find_elements_by_xpath('')) > 0 :
 	# 	button_moreJobs = browser.find_element_by_xpath('')
 	# 	button_moreJobs.click()
 	# 	button_moreJobs = None
-	# 	browser.implicitly_wait(1.7)
+	# 	browser.implicitly_wait(2)
 	# browser.implicitly_wait(2)
 
-	# ------------ Switch to inner iframe if exists ----------
+	# ----------- d. Change number of result per page -------
+	# display = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.NAME, '')))
+	# Select(display).select_by_value('100')
+	# browser.implicitly_wait(2)
+
+	# ------------ e. Switch to inner iframe if exists ----------
 	# browser.switch_to.frame(WebDriverWait(browser, 5).until(EC.presense_of_element_located((By.TAG_NAME, ''))))
 
 	# ------------ Parse all pages of search result ------------
@@ -146,7 +159,6 @@ def parse_job_search_page(browser, keyword, num_of_pages) :
 		if i != 0 :
 			pager.click()
 		browser.implicitly_wait(2)
-		sleep(3)
 
 		table = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '')))
 		jobs = table.find_elements_by_tag_name('tr')
@@ -155,15 +167,20 @@ def parse_job_search_page(browser, keyword, num_of_pages) :
 			url = job.find_element_by_tag_name('a').get_attribute('href')
 			print title + '......' + url + '......Done'
 			location = parse_job_location()
-			if check_if_exists(title, remove_keyword_dict) :
-				tags_list = 'Experienced'
-			else :
-				tags_list = ''
-				tags = tag_job(title, tag_keyword_dict_1)
-				if tags :
-					tags += tag_job(title, tag_keyword_dict_2)
-					tags_list = ','.join(list(set(tags)))
-			result.append([title, url] + location + ['', tags_list])
+
+			# +++++++++++ Tagging Functions (Deprecated) ++++++++++++
+			# if check_if_exists(title, remove_keyword_dict) :
+			# 	tags_list = 'Experienced'
+			# else :
+			# 	tags_list = ''
+			# 	tags = tag_job(title, tag_keyword_dict_1)
+			# 	if tags :
+			# 		tags += tag_job(title, tag_keyword_dict_2)
+			# 		tags_list = ','.join(list(set(tags)))
+			# result.append([title, url] + location + ['', tags_list])
+			# +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+			result.append([title, url] + location)
 		table = None
 	return result 
 
@@ -192,59 +209,63 @@ def parse_job_location(location) :
 	return parsed_loc # <City, Abbreviation, Country>
 
 
-def parse_job_details(browser, worksheet) :
-	browser.set_page_load_timeout(5)
-	raw_data = worksheet.get_all_values()
-	raw_data.pop(0) # Remove header line from spreadsheet
+# ++++++++++++ Job detail parsing (Deprecated) +++++++++++++
+# def parse_job_details(browser, worksheet) :
+# 	browser.set_page_load_timeout(5)
+# 	raw_data = worksheet.get_all_values()
+# 	raw_data.pop(0) # Remove header line from spreadsheet
 
-	for x, val in enumerate(raw_data) :
-		url = val[1]
-		if val[6] == '' :
-			browser.get(url)
-			try :
-				snippet = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "job")))
-				snippet = snippet.get_attribute('innerHTML')
+# 	for x, val in enumerate(raw_data) :
+# 		url = val[1]
+# 		if val[6] == '' :
+# 			browser.get(url)
+# 			try :
+# 				snippet = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "job")))
+# 				snippet = snippet.get_attribute('innerHTML')
 
-				re_img = re.compile('<img\b(.*?)>')
-				snippet = re_img.sub('', snippet)
+# 				re_img = re.compile('<img\b(.*?)>')
+# 				snippet = re_img.sub('', snippet)
 
-				re_href = re.compile('<a\b[^>]*>(.*?)<\/a>')
-				snippet = re_href.sub('', snippet)
+# 				re_href = re.compile('<a\b[^>]*>(.*?)<\/a>')
+# 				snippet = re_href.sub('', snippet)
 
-				re_input = re.compile('<input\b(.*?)>')
-				snippet = re_input.sub('', snippet)
+# 				re_input = re.compile('<input\b(.*?)>')
+# 				snippet = re_input.sub('', snippet)
 
-				# ------------- Trim for necessary part ----------------
-				# soup = BeautifulSoup(snippet)
-				# trimed_data = soup.find_all('div', {'class' : 'contentlinepanel'})[:1]
-				# result = ''.join(str(tag) for tag in trimed_data)
+# 				# ------------- Trim for necessary part ----------------
+# 				# soup = BeautifulSoup(snippet)
+# 				# trimed_data = soup.find_all('div', {'class' : 'contentlinepanel'})[:1]
+# 				# result = ''.join(str(tag) for tag in trimed_data)
 
-			except StaleElementReferenceException:
-				print 'Selenium Error!'
-				return
-			except TimeoutException:
-				print 'Timeout Error!'
-				return
+# 			except StaleElementReferenceException:
+# 				print 'Selenium Error!'
+# 				return
+# 			except TimeoutException:
+# 				print 'Timeout Error!'
+# 				return
 				
-			if snippet :
-				worksheet.update_acell('G'+str(x+2), snippet)
-				print 'Line No.' + str(x+2) + '.......' + url
-			else :
-				print 'Line No.' + str(x+2) + '.......Job not found'
+# 			if snippet :
+# 				worksheet.update_acell('G'+str(x+2), snippet)
+# 				print 'Line No.' + str(x+2) + '.......' + url
+# 			else :
+# 				print 'Line No.' + str(x+2) + '.......Job not found'
 				
-		else :
-			print 'Line No.' + str(x+2) + '.......Skipped'
+# 		else :
+# 			print 'Line No.' + str(x+2) + '.......Skipped'
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 
 if __name__ == '__main__':
 	start_time = time.time()
 
+	credentials_file = 'zheng-6cef143e8ce1.json'
 	url = ''
 	spreadsheet_name = ''
 	worksheet_name = ''
-	local_sheet_name = ''
+	local_sheet_name = '' # Local csv backup file, auto-created
 	num_of_pages = 0 # Number of pages in the search result
-	keyword = '' # Keywords if certain jobs are needed.
+	keyword = '' # Keywords for search(optional)
 	
 
 	# ------ Search page url check --------
@@ -257,7 +278,7 @@ if __name__ == '__main__':
 	parsed_data = parse_job_search_page(browser, keyword, num_of_pages)
 	browser.quit()
 
-	# -------- Download to local csv file --------
+	# -------- Download to local csv file (backup) --------
 	writer = csv.writer(open('Result/'+local_sheet_name, 'w'))
 	for item in parsed_data :
 		writer.writerow(item)
@@ -266,11 +287,11 @@ if __name__ == '__main__':
 	header_line = ['Job Title', 'Job Url', 'City', 'State',	'Country', 'Snippet', 'Tags']
 	write_spreadsheet(spreadsheet_name, worksheet_name, header_line, parsed_data)
 
-	# -------- Parse job detail page (spreadsheet update included)-----------
-	browser = webdriver.Firefox()
-	parse_job_details(browser, ws)
-	browser.quit()
-
+	# ++++++++ Parse job detail page (Deprecated) ++++++++++
+	# browser = webdriver.Firefox()
+	# parse_job_details(browser, ws)
+	# browser.quit()
+	# +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 	print("--- %s seconds ---" % (time.time() - start_time))
 
